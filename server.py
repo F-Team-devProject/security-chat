943c5e6025d89da399d9110172968037b9c30e52
WOKAM Melvine <wokammelvine@gmail.com>
2025-08-28 11:06:59 -0700

    premier fichier

---
+++ server.py
@@ -0,0 +1,40 @@
+from threading import Thread
+import socket
+
+def Send(client):
+    while True:
+     msg = input("->")
+     msg = msg.encode("utf-8")
+     client.send(msg)
+def Reception(client):
+    while True:
+     requete_client = client.recv(500)
+     requete_client = requete_client.decode('utf-8')
+     print(requete_client)
+     if not requete_client :
+        print("CLOSE")
+        break
+
+Host ="192.168.100.79"
+Port = 6390
+
+
+socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+
+socket.bind((Host,Port))
+socket.listen(1)
+
+
+client,ip =socket.accept()
+print("Le client d'ip",ip,"s'est connecter")
+
+envoi = Thread(target=Send,args=[client])
+recep = Thread(target=Reception,args=[client])
+
+envoi.start()
+recep.start()
+
+recep.join()
+
+client.close()
+socket.close()    
